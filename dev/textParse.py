a746358c09a77f7a32d0e85ff8ba42bf3a614ac8
rdiaz21129 <rdiaz21129@gmail.com>
2018-01-09 07:49:57 -0600

    test

---
+++ dev/textParse.py
@@ -0,0 +1,17 @@
+# Ricardo Diaz
+# 20170108
+# Python 3.6
+
+# Import mondules
+import re,sys
+
+# Opens the file "ex_switch_config" in read mode "r"
+switch_config_file = open("ex_switch_config", "r")
+
+# Sends output of open file to a variable called = switch_config_fileOutput
+switch_config_fileOutput = switch_config_file.read()
+
+# Close file
+switch_config_file.close()
+
+print (switch_config_fileOutput)
