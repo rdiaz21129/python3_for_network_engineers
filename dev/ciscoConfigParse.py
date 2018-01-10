268a8fbbc4133a338c1ef6cc70942f5fcf24c77d
Ricardo Diaz <rdiaz@Ricardo-Mac.local>
2018-01-10 05:53:27 -0600

    add readlines to file

--- dev/ciscoConfigParse.py
+++ dev/ciscoConfigParse.py
@@ -9,9 +9,13 @@ import re,sys
 switch_config_file = open("ex_switch_config", "r")
 
 # Sends output of open file to a variable called = switch_config_fileOutput
-switch_config_fileOutput = switch_config_file.read()
+switch_config_fileOutput = switch_config_file.readlines()
+
+#print (switch_config_fileOutput)
+for x in switch_config_fileOutput:
+    #TESTprint (x)
+    if ['address'] in switch_config_fileOutput:
+        print (x)
 
 # Close file
 switch_config_file.close()
-
-print (switch_config_fileOutput)
