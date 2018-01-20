13270b29aa07a57dc1ae34369a7a7830098b90ff
Ricardo Diaz <rdiaz@Ricardo-Mac.local>
2018-01-20 06:29:10 -0600

    morning work

---
+++ dev/cisco_devices/dev_test.py
@@ -0,0 +1,5 @@
+food = ("pizza")
+
+f = open("createdFile.txt", "w")
+f.write("hello,\nopening file and closing it\n")
+f.close()
--- dev/cisco_devices/show_int_status.py
+++ dev/cisco_devices/show_int_status.py
@@ -5,22 +5,41 @@
 from netmiko import ConnectHandler
 from getpass import getpass
 
+
+def cisco_output_to_file():
+    f = open('cisco.txt', 'w')
+    f.write(cisco_output)
+    f.close()
+
+openNewFile = cisco_output_to_file()
+
+# prompt the user to enter an ip address
 ip_addr = input("Enter IP Address: ").strip()
+
+# prompt the user for the password for that IP (above)
 password = getpass()
 
 cisco = {
     'device_type': 'cisco_ios',
     'ip': ip_addr,
-    'username': 'pyclass',
+    'username': 'rdiaz',
     'password': password,
     'port': 22,
     'ssh_config_file': '~/.ssh/config',
     'verbose': False,
 }
 
+# Connects to ip_addr referanced in the cisco dictionary and uses the password also found on the dictionary
 net_connect = ConnectHandler(**cisco)
-output = net_connect.send_command('show users')
-print output
+
+# Creates a var that is the output/result of (connecting to the device via ssh, and the commands that are send)
+cisco_output = net_connect.send_command('show int status')
+
+# Print output of the cisco output on the terminal
+#print (output)
+
+print (openNewFile)
+
 print
-print net_connect.find_prompt()
+print (net_connect.find_prompt())
 print
