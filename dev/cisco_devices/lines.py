a4c3c8a58c2a0ed0c6c543de46729d9629ca76cf
rdiaz21129 <rdiaz21129@gmail.com>
2018-01-21 09:05:21 -0600

    rd

--- dev/cisco_devices/lines.py
+++ dev/cisco_devices/lines.py
@@ -4,20 +4,20 @@ user_range_input = int(input("Enter number of lines you want created: "))
 
 # Define function that will create lines
 # 1
-def createLines(user_range_input):
+def createLines(y):
     x_count = 0
-    for i in range(user_range_input):
+    for i in range(y):
         x_count+=1
         print ("This is line", x_count)
-
+        
 
 functionOneOutput = createLines
 
-print ("***" * 8)
+#print ("***" * 8)
 
-print (functionOneOutput(user_range_input))
-'''
+#print (functionOneOutput(user_range_input))
+#'''
 f = open("createdFile.txt", "w")
 f.write(str(functionOneOutput(user_range_input)))
 f.close()
-'''
+#'''
