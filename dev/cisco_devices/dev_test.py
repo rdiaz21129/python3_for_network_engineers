28cbf430d5283a77af2d1f6957482f73d738896e
rdiaz21129 <rdiaz21129@gmail.com>
2018-01-22 10:35:13 -0600

    rd

--- dev/cisco_devices/dev_test.py
+++ dev/cisco_devices/dev_test.py
@@ -1,5 +1,21 @@
+
+f = open('serial_numbers.txt', 'r')
+f_output = f.readlines()
+f.close()
+
+#print (f_output)
+x = ('FDO1441P0CR')
+x_list = ['xxricardo', 'carlosxxx', 'lucero']
+for line in f_output:
+    line = line.strip() # remove blank lines
+    #print (line)
+    for x_list in line:
+        print (x_list)
+
+'''
 food = ("pizza")
 
 f = open("createdFile.txt", "w")
 f.write("hello,\nopening file and closing it\n")
 f.close()
+'''
