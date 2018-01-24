ddb1a500d92740354e2bcbc7adcf19913b1dad16
rdiaz21129 <rdiaz21129@gmail.com>
2018-01-23 23:11:42 -0600

    rd

--- dev/cisco_devices/dev_test.py
+++ dev/cisco_devices/dev_test.py
@@ -1,29 +1,27 @@
 
 #https://stackoverflow.com/questions/8583615/how-to-check-if-a-line-has-one-of-the-strings-in-a-list
 #https://stackoverflow.com/questions/29106881/check-if-any-of-the-items-of-a-list-is-in-a-line-of-a-file-if-not-then-write-t
-my_unwanted_words = set(['ssh', 'snmp', 'etc'])
-with open("infile_test.txt", 'r') as infile, open("newfile.txt", 'w') as newopen:
-    lines = infile.readlines()
-    [newopen.write(line) for line in lines if not (set(line.split()) & my_unwanted_words)]
 
 
 
 
-'''
+
+# OPen and close file
 f = open('serial_numbers.txt', 'r')
 f_output = f.readlines()
 f.close()
 
-#print (f_output)
-name = ('ricardo')
+# Create lists that will be used in for loops
 x = ['pizza', 'tacos', 'beer']
+
+# For variable in f_output
 for line in f_output:
     line = line.strip() # remove blank lines
-    #print (line)
-    if x in line:
-        print (x)
 
+    if any(newvar in line for newvar in x):
+        print (line)
 
+'''
 food = ("pizza")
 
 f = open("createdFile.txt", "w")
